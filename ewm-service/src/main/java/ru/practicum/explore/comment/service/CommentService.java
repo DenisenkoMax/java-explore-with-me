05a68f5562dc4973fa7a6a542dea21a410cdca0a
Max <denmax@mail.ru>
2022-12-01 14:38:15 +0700

    На проверку напарнику/ особенность (feature)

--- ewm-service/src/main/java/ru/practicum/explore/comment/service/CommentService.java
+++ ewm-service/src/main/java/ru/practicum/explore/comment/service/CommentService.java
@@ -11,15 +11,21 @@ public interface CommentService {
     CommentDto addComment(Long userId, Long eventId, NewCommentDto newCommentDto);
 
     CommentDto updateComment(Long userId, UpdateCommentDto updateCommentDto);
+
     CommentDto updateCommentAdmin(UpdateCommentDto updateCommentDto);
 
     List<CommentDto> getAllCommentsPrivate(Long userId, Boolean onlyPublished, int from, int size);
 
     CommentDto getCommentByIdPrivate(Long userId, Long commentId);
+
     CommentDto publishCommentUserPrivate(Long userId, Long commentId);
+
     void deleteCommentByIdPrivate(Long userId, Long commentId);
+
     List<ShortCommentDto> getAllCommentsByEventPub(Long eventId, int from, int size);
+
     ShortCommentDto getCommentByIdPublic(Long commentId);
-    List<CommentDto>  getAllCommentsAdmin(Long ownerId, Long commenterId, Boolean onlyPublished, int from, int size);
+
+    List<CommentDto> getAllCommentsAdmin(Long ownerId, Long commenterId, Boolean onlyPublished, int from, int size);
 
 }
