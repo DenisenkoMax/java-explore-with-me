4e9e4e2ff2aaafed7c8dc11a811dc3d023ac8ae6
Max <denmax@mail.ru>
2022-12-01 13:59:21 +0700

    На проверку напарнику/ особенность (feature)

--- ewm-service/src/main/java/ru/practicum/explore/comment/service/CommentServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/explore/comment/service/CommentServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+
 import ru.practicum.explore.comment.dto.*;
 import ru.practicum.explore.comment.model.Comment;
 import ru.practicum.explore.comment.repository.CommentRepositoryJpa;
